8c72b5d626ac944beb0bd418912418f6066c069b
Saad Ahmad <saadahmad.cs@gmail.com>
2024-06-04 17:54:06 +0500

    minor updates

--- firebase.js
+++ firebase.js
@@ -1,9 +1,18 @@
 import { initializeApp, getApp, getApps } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
-import environment from "./environments/environment";
+import dotenv from 'dotenv';
+dotenv.config();
 
-const firebaseConfig = environment.firebaseConfig;
+const firebaseConfig = {
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID
+};
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 
@@ -12,24 +21,3 @@ const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
 
 export { db, auth, provider };
-
-
-// import firebase from "firebase"
-
-// const firebaseConfig = {
-//     apiKey: "AIzaSyDHXT6MVJQGPNmDGDPJkon92iJIbOCJtZ0",
-//     authDomain: "chit-chat-de826.firebaseapp.com",
-//     projectId: "chit-chat-de826",
-//     storageBucket: "chit-chat-de826.appspot.com",
-//     messagingSenderId: "1001235024086",
-//     appId: "1:1001235024086:web:3288458f1117c41a45e177",
-//     measurementId: "G-DY6300PMCT"
-//   };
-
-// const app = !firebase.apps.length ? firebase.initializeApp(firebaseConfig) : firebase.app();
-
-// const db = app.firestore();
-// const auth = app.auth();
-// const provider = new firebase.auth.GoogleAuthProvider();
-
-// export {db, auth, provider};
